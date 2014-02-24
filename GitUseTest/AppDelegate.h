5fb271fb09bb105ad1f0212c4675437a331c161b
linan <linan@thetigerparty.com>
2014-02-24 00:11:26 -0500

    Initial Commit

---
+++ GitUseTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseTest
+//
+//  Created by linan on 2/24/14.
+//  Copyright (c) 2014 linan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseTest
+//
+//  Created by linan on 2/24/14.
+//  Copyright (c) 2014 linan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
