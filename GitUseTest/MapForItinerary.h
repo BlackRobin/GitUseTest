00205eeffe90a59661491c15c8092ffc6b46da56
linan <linan@thetigerparty.com>
2014-02-24 09:33:50 -0500

    (Branch Test) - Create new classes fix map itinerary feature

---
+++ GitUseTest/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitUseTest
+//
+//  Created by linan on 2/24/14.
+//  Copyright (c) 2014 linan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
---
+++ GitUseTest/MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.m
+//  GitUseTest
+//
+//  Created by linan on 2/24/14.
+//  Copyright (c) 2014 linan. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+@implementation MapForItinerary
+
+@end
